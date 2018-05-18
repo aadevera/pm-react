856d0e6e43cd9f8295271f3106889ae0785a73f2
aadevera <aadevera@up.edu.ph>
2018-05-18 20:31:41 +0800

    Working Edit Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,6 +11,36 @@ class Profile extends Component {
             userName: '',
             userData: {}
         }
+        this.editProfile = this.editProfile.bind(this)
+    }
+    async editProfile(e) {
+        
+        const data = {
+            userid: this.state.userId,
+            email: document.getElementById("editemail").value,
+            password: document.getElementById('editpass').value,
+            name: document.getElementById('editname').value
+        }
+        try {
+            const response = await fetch ('http://localhost:8000/user/edit', {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            const result = await response.json()
+            alert(result.message)
+
+            const get_options = { headers: { 'Content-Type': 'application/json' }}
+            const response_getuser = await fetch('http://localhost:8000/user/find-by-id/' + this.state.userId, get_options)
+            const result_getuser = await response_getuser.json()
+            this.setState({userData: result_getuser.data})
+        } catch (e) {
+            throw new Error(e)
+        }
+        
+
     } 
     async componentWillMount () {
         
@@ -19,7 +49,6 @@ class Profile extends Component {
             const get_options = { headers: { 'Content-Type': 'application/json' }}
             const response_getdata = await fetch('http://localhost:8000/getdata', tok_options)
             const result_getdata = await response_getdata.json()
-            
             if (!result_getdata.success) {
                 alert (result_getdata.message)
                 localStorage.removeItem('token')
@@ -31,6 +60,7 @@ class Profile extends Component {
                 userName: result_getdata.data.name
             }
             this.setState(state_getdata)
+            
             //await new Promise ((resolve) => { this.setState(state_getdata, resolve) })
             const response = await fetch('http://localhost:8000/user/find-by-id/' + this.state.userId, get_options)
             const result = await response.json()
@@ -50,23 +80,22 @@ class Profile extends Component {
         }
         return (
             <div>
-                
-                <form className="w-50 align-middle mx-auto">
+                <form onSubmit={this.editProfile} className="w-50 align-middle mx-auto" >
                     <h4>Edit Profile</h4>
                     <hr /> 
                     <div className="row">
                         <div className="form-group col">
-                            <label htmlFor="editemail">Email</label>
-                            <input type="email" className="form-control" id="editemail" placeholder={this.state.userData.email} required/>
+                            <label>Email</label>
+                            <input type="email" className="form-control" id="editemail" placeholder={this.state.userData.email} required />
                         </div>
                         <div className="form-group col">
-                            <label htmlFor="editpass">Password</label>
-                            <input type="password" className="form-control" id="editpass" placeholder="Password" required/>
+                            <label >Password</label>
+                            <input type="password" className="form-control" id="editpass" placeholder="Password" required />
                         </div>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="editname">Email</label>
-                        <input type="text"  className="form-control" id="editname" placeholder={this.state.userData.name} required/>
+                        <label >Name</label>
+                        <input type="text" className="form-control" id="editname" placeholder={this.state.userData.name} required />
                     </div>
                     <div className="form-group">
                         <button type="submit" className="btn btn-primary float-right">Save Profile</button>
